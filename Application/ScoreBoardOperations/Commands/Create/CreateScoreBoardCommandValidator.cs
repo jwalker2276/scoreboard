10383aff7a3ee6585182d75470670f5c2d953d3d
Jordan Walker <jwalkercreations@gmail.com>
2022-11-27 11:39:13 -0600

    Added scoreboard create validation.

--- Application/ScoreBoardOperations/Commands/Create/CreateScoreBoardCommandValidator.cs
+++ Application/ScoreBoardOperations/Commands/Create/CreateScoreBoardCommandValidator.cs
@@ -1,8 +1,35 @@
-﻿using FluentValidation;
+﻿using Application.Common.Validation;
+using FluentValidation;
 
 namespace Application.ScoreBoardOperations.Commands.Create;
 
 public class CreateScoreBoardCommandValidator : AbstractValidator<CreateScoreBoardCommand>
 {
+    public CreateScoreBoardCommandValidator()
+    {
+        RuleFor(sb => sb.GameId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .Must(ValidationHelper.IsAGuid);
 
+        RuleFor(sb => sb.Name)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(256)
+                .Must(ValidationHelper.HaveAcceptableCharacters);
+
+        RuleFor(sb => sb.MaxNumberOfScores)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(1)
+                .LessThanOrEqualTo(100);
+
+        RuleFor(sb => sb.CreatedBy)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(256)
+                .Must(ValidationHelper.HaveAcceptableCharacters);
+    }
 }
