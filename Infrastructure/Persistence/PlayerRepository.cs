dfe3989d278853498bf1c9c43beccd8120d5d28d
Jordan Walker <jwalkercreations@gmail.com>
2022-11-27 14:12:14 -0600

    Added new get by name method to player repository class.

--- Infrastructure/Persistence/PlayerRepository.cs
+++ Infrastructure/Persistence/PlayerRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Persistence;
 using Domain.PlayerModels.Entities;
+using Infrastructure.CustomModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence;
@@ -10,17 +11,19 @@ public class PlayerRepository : IRepository<Player>
 
     private readonly DbSet<Player> _dbPlayers;
 
+    private readonly DbSet<PlayerNameBlackList> _dbPlayerNameBlackLists;
+
     public PlayerRepository(DatabaseContext databaseContext)
     {
         _databaseContext = databaseContext;
         _dbPlayers = _databaseContext.Set<Player>();
+        _dbPlayerNameBlackLists = _databaseContext.Set<PlayerNameBlackList>();
     }
 
     public void Create(Player entity)
     {
         throw new NotImplementedException();
     }
-
     public Task<List<Player>> GetAll(CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
@@ -31,10 +34,16 @@ public class PlayerRepository : IRepository<Player>
         throw new NotImplementedException();
     }
 
+    public Task<Player?> GetByName(string name, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
     public Task<Player?> FindAndUpdate(Player updatedEntity, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
+
     public Task<Player?> FindAndDelete(Guid id, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
